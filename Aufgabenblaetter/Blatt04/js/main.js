33371b5ab5f91a338cd580977ecb2278d0a3d19d
Kaito <ben180701@gmail.com>
2022-05-30 22:05:13 +0200

    testing lang

--- Aufgabenblaetter/Blatt04/js/main.js
+++ Aufgabenblaetter/Blatt04/js/main.js
@@ -3,5 +3,5 @@ import * as Models from "./models.js";
 //id, titel, kurzbeschreibung, logoUrl, projektStart
 
 let a = new Models.Projekt("1234", "Ein Titel", "Eine Kurzbeschreibung", "http://www.logo.de", 0);
-
+console.log(navigator.language);
 console.log(a);
