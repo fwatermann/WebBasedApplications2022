c2544f97ab2a36f6e2a93a136ea3946672d5c8bc
Finn Watermann <finn.watermann@fh-bielefeld.de>
2022-05-30 22:52:07 +0200

    Added sort function

--- Aufgabenblaetter/Blatt04/js/util.js
+++ Aufgabenblaetter/Blatt04/js/util.js
@@ -4,3 +4,21 @@ export function calcProjectRuntime(project){
     
 }
 
+export function sortProjects(projects, sortBy) {
+
+    switch(sortBy) {
+        case "startDate":
+            projects.sort((a, b) => {
+                return a.projektStart - b.projektStart;
+            });
+        break;
+        case "runtime":
+            projects.sort((a, b) => {
+                return calcProjectRuntime(a) - calcProjectRuntime(b);
+            });
+        break;
+        default:
+            return projects;
+    }
+
+}
